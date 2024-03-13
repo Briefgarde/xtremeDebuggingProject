bdae6cefb5ee12005c1d07774e73f32a0cf66a6c
Briefgarde <nemo.vollert@he-arc.ch>
2024-03-13 19:12:55 +0100

    created tests for getParentDroitAllocation

--- src/test/java/ch/hearc/cafheg/business/allocations/AllocationServiceTest.java
+++ src/test/java/ch/hearc/cafheg/business/allocations/AllocationServiceTest.java
@@ -12,10 +12,14 @@ import java.util.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class AllocationServiceTest {
+
+  private static final String PARENT_1 = "Parent1";
+  private static final String PARENT_2 = "Parent2";
 
   private AllocationService allocationService;
 
@@ -29,48 +33,49 @@ class AllocationServiceTest {
 
     allocationService = new AllocationService(allocataireMapper, allocationMapper);
   }
+//
+//  @Test
+//  void findAllAllocataires_GivenEmptyAllocataires_ShouldBeEmpty() {
+//    Mockito.when(allocataireMapper.findAll("Geiser")).thenReturn(Collections.emptyList());
+//    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
+//    assertThat(all).isEmpty();
+//  }
+//
+//  @Test
+//  void findAllAllocataires_Given2Geiser_ShouldBe2() {
+//    Mockito.when(allocataireMapper.findAll("Geiser"))
+//        .thenReturn(Arrays.asList(new Allocataire(new NoAVS("1000-2000"), "Geiser", "Arnaud"),
+//            new Allocataire(new NoAVS("1000-2001"), "Geiser", "Aurélie")));
+//    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
+//    assertAll(() -> assertThat(all.size()).isEqualTo(2),
+//        () -> assertThat(all.get(0).getNoAVS()).isEqualTo(new NoAVS("1000-2000")),
+//        () -> assertThat(all.get(0).getNom()).isEqualTo("Geiser"),
+//        () -> assertThat(all.get(0).getPrenom()).isEqualTo("Arnaud"),
+//        () -> assertThat(all.get(1).getNoAVS()).isEqualTo(new NoAVS("1000-2001")),
+//        () -> assertThat(all.get(1).getNom()).isEqualTo("Geiser"),
+//        () -> assertThat(all.get(1).getPrenom()).isEqualTo("Aurélie"));
+//  }
+//
+//  @Test
+//  void findAllocationsActuelles() {
+//    Mockito.when(allocationMapper.findAll())
+//        .thenReturn(Arrays.asList(new Allocation(new Montant(new BigDecimal(1000)), Canton.NE,
+//            LocalDate.now(), null), new Allocation(new Montant(new BigDecimal(2000)), Canton.FR,
+//            LocalDate.now(), null)));
+//    List<Allocation> all = allocationService.findAllocationsActuelles();
+//    assertAll(() -> assertThat(all.size()).isEqualTo(2),
+//        () -> assertThat(all.get(0).getMontant()).isEqualTo(new Montant(new BigDecimal(1000))),
+//        () -> assertThat(all.get(0).getCanton()).isEqualTo(Canton.NE),
+//        () -> assertThat(all.get(0).getDebut()).isEqualTo(LocalDate.now()),
+//        () -> assertThat(all.get(0).getFin()).isNull(),
+//        () -> assertThat(all.get(1).getMontant()).isEqualTo(new Montant(new BigDecimal(2000))),
+//        () -> assertThat(all.get(1).getCanton()).isEqualTo(Canton.FR),
+//        () -> assertThat(all.get(1).getDebut()).isEqualTo(LocalDate.now()),
+//        () -> assertThat(all.get(1).getFin()).isNull());
+//  }
 
   @Test
-  void findAllAllocataires_GivenEmptyAllocataires_ShouldBeEmpty() {
-    Mockito.when(allocataireMapper.findAll("Geiser")).thenReturn(Collections.emptyList());
-    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
-    assertThat(all).isEmpty();
-  }
-
-  @Test
-  void findAllAllocataires_Given2Geiser_ShouldBe2() {
-    Mockito.when(allocataireMapper.findAll("Geiser"))
-        .thenReturn(Arrays.asList(new Allocataire(new NoAVS("1000-2000"), "Geiser", "Arnaud"),
-            new Allocataire(new NoAVS("1000-2001"), "Geiser", "Aurélie")));
-    List<Allocataire> all = allocationService.findAllAllocataires("Geiser");
-    assertAll(() -> assertThat(all.size()).isEqualTo(2),
-        () -> assertThat(all.get(0).getNoAVS()).isEqualTo(new NoAVS("1000-2000")),
-        () -> assertThat(all.get(0).getNom()).isEqualTo("Geiser"),
-        () -> assertThat(all.get(0).getPrenom()).isEqualTo("Arnaud"),
-        () -> assertThat(all.get(1).getNoAVS()).isEqualTo(new NoAVS("1000-2001")),
-        () -> assertThat(all.get(1).getNom()).isEqualTo("Geiser"),
-        () -> assertThat(all.get(1).getPrenom()).isEqualTo("Aurélie"));
-  }
-
-  @Test
-  void findAllocationsActuelles() {
-    Mockito.when(allocationMapper.findAll())
-        .thenReturn(Arrays.asList(new Allocation(new Montant(new BigDecimal(1000)), Canton.NE,
-            LocalDate.now(), null), new Allocation(new Montant(new BigDecimal(2000)), Canton.FR,
-            LocalDate.now(), null)));
-    List<Allocation> all = allocationService.findAllocationsActuelles();
-    assertAll(() -> assertThat(all.size()).isEqualTo(2),
-        () -> assertThat(all.get(0).getMontant()).isEqualTo(new Montant(new BigDecimal(1000))),
-        () -> assertThat(all.get(0).getCanton()).isEqualTo(Canton.NE),
-        () -> assertThat(all.get(0).getDebut()).isEqualTo(LocalDate.now()),
-        () -> assertThat(all.get(0).getFin()).isNull(),
-        () -> assertThat(all.get(1).getMontant()).isEqualTo(new Montant(new BigDecimal(2000))),
-        () -> assertThat(all.get(1).getCanton()).isEqualTo(Canton.FR),
-        () -> assertThat(all.get(1).getDebut()).isEqualTo(LocalDate.now()),
-        () -> assertThat(all.get(1).getFin()).isNull());
-  }
-
-  @Test
+  @DisplayName("If parent 1 is active and not parent 2, should be P1")
   public void getParentDroitAllocation_GivenParent1Active_ShouldReturnParent1() {
     Map<String, Object> parameters = new HashMap<>();
     parameters.put("parent1ActiviteLucrative", true);
@@ -78,10 +83,11 @@ class AllocationServiceTest {
 
     String result = allocationService.getParentDroitAllocation(parameters);
 
-    Assertions.assertEquals("Parent1", result);
+    Assertions.assertEquals(PARENT_1, result);
   }
 
   @Test
+  @DisplayName("If parent 1 is NOT active and parent 2 IS, should be P2")
   public void getParentDroitAllocation_GivenParent2Active_ShouldReturnParent2() {
     Map<String, Object> parameters = new HashMap<>();
     parameters.put("parent1ActiviteLucrative", false);
@@ -89,33 +95,236 @@ class AllocationServiceTest {
 
     String result = allocationService.getParentDroitAllocation(parameters);
 
-    assertEquals("Parent2", result);
+    assertEquals(PARENT_2, result);
   }
 
   @Test
-  public void getParentDroitAllocation_GivenBothParentsActiveAndParent1HigherSalary_ShouldReturnParent1() {
+  public void getParentDroitAllocation_GivenParentsActive_and_P1_WithAuthority_ShouldBeP1() {
     Map<String, Object> parameters = new HashMap<>();
     parameters.put("parent1ActiviteLucrative", true);
     parameters.put("parent2ActiviteLucrative", true);
-    parameters.put("parent1Salaire", BigDecimal.valueOf(5000));
-    parameters.put("parent2Salaire", BigDecimal.valueOf(3000));
-
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", false);
     String result = allocationService.getParentDroitAllocation(parameters);
 
-    assertEquals("Parent1", result);
+    assertEquals(PARENT_1, result);
   }
 
   @Test
-  public void getParentDroitAllocation_GivenBothParentsActiveAndParent2HigherSalary_ShouldReturnParent2() {
+  public void getParentDroitAllocation_GivenParentsActive_and_P2_WithAuthority_ShouldBeP2() {
     Map<String, Object> parameters = new HashMap<>();
     parameters.put("parent1ActiviteLucrative", true);
     parameters.put("parent2ActiviteLucrative", true);
-    parameters.put("parent1Salaire", BigDecimal.valueOf(3000));
-    parameters.put("parent2Salaire", BigDecimal.valueOf(5000));
-
+    parameters.put("parent1WithParentalAuthority", false);
+    parameters.put("parent2WithParentalAuthority", true);
     String result = allocationService.getParentDroitAllocation(parameters);
 
-    assertEquals("Parent2", result);
+    assertEquals(PARENT_2, result);
   }
 
+  @Test
+  public void getParentDroitAllocation_GivenParentsSeparatedAndChildWithP1_ShouldBeP1() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", false);
+    parameters.put("enfantResidence", 1);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_1, result);
+  }
+  @Test
+  public void getParentDroitAllocation_GivenParentsSeparatedAndChildWithP2_ShouldBeP2() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", false);
+    parameters.put("enfantResidence", 2);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_2, result);
+  }
+
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndP1WorkInSameCantonAsChild_ShouldBeP1() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", true);
+    parameters.put("P2WorkInChildCanton", false);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_1, result);
+  }
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndP2WorkInSameCantonAsChild_ShouldBeP2() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", false);
+    parameters.put("P2WorkInChildCanton", true);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_2, result);
+  }
+
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndBothWorkInCantonOfChildButP1IsRich_ShouldBeP1() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", true);
+    parameters.put("P2WorkInChildCanton", true);
+    parameters.put("parent1Salaire", 10);
+    parameters.put("parent2Salaire", 5);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_1, result);
+  }
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndBothWorkInCantonOfChildButP2IsRich_ShouldBeP2() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", true);
+    parameters.put("P2WorkInChildCanton", true);
+    parameters.put("parent1Salaire", 10);
+    parameters.put("parent2Salaire", 15);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_2, result);
+  }
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndAreSalariedAndP2isRich_ShouldBeP2() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", false);
+    parameters.put("P2WorkInChildCanton", false);
+    parameters.put("P1Salaried", true);
+    parameters.put("P2Salaried", true);
+    parameters.put("parent1Salaire", 10);
+    parameters.put("parent2Salaire", 15);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_2, result);
+  }
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndAreSalariedAndP1isRich_ShouldBeP1() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", false);
+    parameters.put("P2WorkInChildCanton", false);
+    parameters.put("P1Salaried", true);
+    parameters.put("P2Salaried", true);
+    parameters.put("parent1Salaire", 10);
+    parameters.put("parent2Salaire", 5);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_1, result);
+  }
+
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndP1IsSalaried_ShouldBeP1() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", false);
+    parameters.put("P2WorkInChildCanton", false);
+    parameters.put("P1Salaried", true);
+    parameters.put("P2Salaried", false);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_1, result);
+  }
+  public void getParentDroitAllocation_GivenParentsTogetherAndP2IsSalaried_ShouldBeP2() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", false);
+    parameters.put("P2WorkInChildCanton", false);
+    parameters.put("P1Salaried", false);
+    parameters.put("P2Salaried", true);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_2, result);
+  }
+
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndAreIndependantAndP1isRich_ShouldBeP1() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", false);
+    parameters.put("P2WorkInChildCanton", false);
+    parameters.put("P1Salaried", false);
+    parameters.put("P2Salaried", false);
+    parameters.put("parent1Salaire", 10);
+    parameters.put("parent2Salaire", 5);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_1, result);
+  }
+
+  @Test
+  public void getParentDroitAllocation_GivenParentsTogetherAndAreIndependantAndP2isRich_ShouldBeP2() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", true);
+    parameters.put("parent2ActiviteLucrative", true);
+    parameters.put("parent1WithParentalAuthority", true);
+    parameters.put("parent2WithParentalAuthority", true);
+    parameters.put("parentsEnsemble", true);
+    parameters.put("P1WorkInChildCanton", false);
+    parameters.put("P2WorkInChildCanton", false);
+    parameters.put("P1Salaried", false);
+    parameters.put("P2Salaried", false);
+    parameters.put("parent1Salaire", 10);
+    parameters.put("parent2Salaire", 15);
+    String result = allocationService.getParentDroitAllocation(parameters);
+
+    assertEquals(PARENT_2, result);
+  }
+
+  @Test
+  @DisplayName("If both parents don't work, method should give an error.")
+  public void getParentDroitAllocations_GivenNoParentHaveAL_ShouldBeError() {
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("parent1ActiviteLucrative", false);
+    parameters.put("parent2ActiviteLucrative", false);
+
+    assertThrows(IllegalArgumentException.class,
+            ()-> allocationService.getParentDroitAllocation(parameters));
+  }
 }
